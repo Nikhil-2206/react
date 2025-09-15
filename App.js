f5024037fa40e4f1847ad1d69d8ebbfd64db7c1a
Nikhil-2206 <nihkil80029@gmail.com>
2025-09-15 23:26:06 +0530

    Add my first React component

---
+++ App.js
@@ -0,0 +1,20 @@
+// App.js
+
+import React from 'react';
+
+// Define a functional component named App
+// The component's name should start with a capital letter
+function App() {
+  return (
+    // This is JSX (JavaScript XML), which looks like HTML
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello, React!</h1>
+        <p>This is my first React component.</p>
+      </header>
+    </div>
+  );
+}
+
+// Export the component so it can be used in other files
+export default App;
